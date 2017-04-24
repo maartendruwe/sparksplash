34dc12b99cf82e344b2d1a196056b6962cd86566
Maarten Druwe <mdruwe@cisco.com>
2017-04-24 10:51:05 +0200

    Updated code for angularjs >1.3 (so creating an app module with functions)

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,12 +1,5 @@
-function meetupsController($scope) {
+var meetupsApp = angular.module('meetupsApp', []);
+meetupsApp.controller('meetupsController', function ($scope) {
 	$scope.meetupsCount = 10;
-	$scope.meetups = [
-		{ name: "MEAN SF Developers" },
-		{ name: "Some other meetups" }
-	]
+});
 
-	$scope.createMeetup = function () {
-		$scope.meetups.push({name: $scope.meetupName});
-		$scope.meetupName = '';
-	}
-}
