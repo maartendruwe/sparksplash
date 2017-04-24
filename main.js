81f058450af4455f46db2145fd5f8d9d2cc19422
Maarten Druwe <mdruwe@cisco.com>
2017-04-24 11:53:41 +0200

    server/controllers

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var express = require('express'),
-	app 	= express();
+	app 	= express(),
+	meetupsController = require('./server/controllers/meetups-controller');
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.use('/js', express.static(__dirname + '/client/js')); //everytime someone requests /js file, routed to /client/js folder
+
+app.post('/api/meetups', meetupsController.create);
 
 app.listen(3000, function() {
 	console.log('I\'m Listening ...');
