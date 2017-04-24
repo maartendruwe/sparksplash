ede74b4730bbf40ab4a432a64abc41accce7b78a
Maarten Druwe <mdruwe@cisco.com>
2017-04-24 11:36:04 +0200

    created app.js, did some restructuring

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,10 +1,8 @@
-var meetupsApp = angular.module('meetupsApp', []);
-meetupsApp.controller('meetupsController', function ($scope) {
+app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resource) {
 	$scope.meetupsCount = 10;
 	$scope.meetups = [];
 	$scope.createMeetup = function () {
 		$scope.meetups.push({name: $scope.meetupName});
 		$scope.meetupName = '';
 	}
-});
-
+}]);
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/client/views/index.html');
 });
 
-app.use('/js', express.static(__dirname + '/client/js')); //everytime someone requests .js file, routed to /client/js folder
+app.use('/js', express.static(__dirname + '/client/js')); //everytime someone requests /js file, routed to /client/js folder
 
 app.listen(3000, function() {
 	console.log('I\'m Listening ...');
