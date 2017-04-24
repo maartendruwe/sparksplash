a489ab719036188634959d84295d542e4127cada
Maarten Druwe <mdruwe@cisco.com>
2017-04-24 11:01:07 +0200

    updates

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,5 +1,10 @@
 var meetupsApp = angular.module('meetupsApp', []);
 meetupsApp.controller('meetupsController', function ($scope) {
 	$scope.meetupsCount = 10;
+	$scope.meetups = [];
+	$scope.createMeetup = function () {
+		$scope.meetups.push({name: $scope.meetupName});
+		$scope.meetupName = '';
+	}
 });
 
