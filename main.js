690840be2433b1f2d68292b8dd26d852225af3c0
Maarten Druwe <mdruwe@cisco.com>
2017-04-24 14:17:52 +0200

    changed MongoClient for mongoose

--- main.js
+++ main.js
@@ -6,16 +6,18 @@ var express = require('express'),
 	format  	= require('util').format,
 	meetupsController = require('./server/controllers/meetups-controller.js');
 
-MongoClient.connect('mongodb://admin:xKlMFKbba94OSz9H@merakisplash-shard-00-00-1fh1l.mongodb.net:27017,merakisplash-shard-00-01-1fh1l.mongodb.net:27017,merakisplash-shard-00-02-1fh1l.mongodb.net:27017/admin?ssl=true&replicaSet=MerakiSplash-shard-0&authSource=admin', function(err, db) {
+/*MongoClient.connect('mongodb://admin:xKlMFKbba94OSz9H@merakisplash-shard-00-00-1fh1l.mongodb.net:27017,merakisplash-shard-00-01-1fh1l.mongodb.net:27017,merakisplash-shard-00-02-1fh1l.mongodb.net:27017/admin?ssl=true&replicaSet=MerakiSplash-shard-0&authSource=admin', function(err, db) {
 	if (err) {
 		throw err;
 	} else {
 		console.log("successfully connected to the database");
 	}
 	db.close();
-})
+})*/
 
-//mongoose.connect('mongodb://localhost:27017/mean-demo');
+// connection string using mongoose:
+var uri = 'mongodb://admin:xKlMFKbba94OSz9H@merakisplash-shard-00-00-1fh1l.mongodb.net:27017,merakisplash-shard-00-01-1fh1l.mongodb.net:27017,merakisplash-shard-00-02-1fh1l.mongodb.net:27017/admin?ssl=true&replicaSet=MerakiSplash-shard-0&authSource=admin';
+mongoose.connect(uri);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
