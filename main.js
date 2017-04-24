c5915a5ea125b91060a70f6d6c6f3feb5ab7cfe5
Maarten Druwe <mdruwe@cisco.com>
2017-04-24 17:15:04 +0200

    mongodb change

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var express = require('express'),
 })*/
 
 // connection string using mongoose:
-var uri = 'mongodb://admin:xKlMFKbba94OSz9H@merakisplash-shard-00-00-1fh1l.mongodb.net:27017,merakisplash-shard-00-01-1fh1l.mongodb.net:27017,merakisplash-shard-00-02-1fh1l.mongodb.net:27017/admin?ssl=true&replicaSet=MerakiSplash-shard-0&authSource=admin&node-test';
+var uri = 'mongodb://webapp:C9DPqemcIVR8LbzG@merakisplash-shard-00-00-1fh1l.mongodb.net:27017,merakisplash-shard-00-01-1fh1l.mongodb.net:27017,merakisplash-shard-00-02-1fh1l.mongodb.net:27017/admin?ssl=true&replicaSet=MerakiSplash-shard-0&authSource=admin&node-test';
 mongoose.connect(uri);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
